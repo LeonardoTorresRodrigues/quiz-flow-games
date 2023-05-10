e2e6ee76bad64c5fc7281babdcd0ed1df6f4101f
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-05-10 13:10:48 -0300

    feat: top month section component

---
+++ src/components/TopMonth.jsx
@@ -0,0 +1,10 @@
+import MainCard from "@/components/MainCard";
+
+export default function TopMonth() {
+  return (
+    <main className="flex-1 p-6">
+      <h2 className="text-4xl">Top do mês</h2>
+      <MainCard />
+    </main>
+  )
+}
